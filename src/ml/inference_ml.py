be7e9ece151639ce0b9f1df4b1d479b69b6e8b3f
llpscc <l.pascenco@gmail.com>
2026-03-19 01:19:14 +0200

    Update inference_ml.py

--- src/ml/inference_ml.py
+++ src/ml/inference_ml.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 
 
 def build_history(train_df):
-
+    # строит историю по тренировочным данным для построения лагов и роллингов для теста 
     return (
         train_df.sort_values("date")
         .groupby(["store_nbr", "item_nbr"])["unit_sales"]
@@ -14,17 +14,18 @@ def build_history(train_df):
 
 
 def recursive_forecast(X_test, X_train, model, history, cat_cols):
-
+    
     history = history.copy()
     predictions = []
-
+    
+    # идем по всем датам в test
     for d in tqdm(sorted(X_test["date"].unique()), desc="Predicting"):
 
         day_df = X_test[X_test["date"] == d].copy()
 
         lag1, lag7, lag14, lag28 = [], [], [], []
         roll7, roll14 = [], []
-
+        # считаем и добавляем лаги и роллинги
         for row in day_df.itertuples():
 
             key = (row.store_nbr, row.item_nbr)
@@ -50,18 +51,19 @@ def recursive_forecast(X_test, X_train, model, history, cat_cols):
         day_df["rolling_mean_14"] = roll14
 
         X_day = day_df[X_train.columns].copy()
-
+        # меняем тип данных на категорию
         for col in cat_cols:
             X_day[col] = X_day[col].astype("category")
             X_day[col] = X_day[col].cat.set_categories(
                 X_train[col].cat.categories
             )
-
+        # предсказание отдельно для каждого дня
         pred = model.predict(X_day)
         pred = np.clip(pred, 0, None)
 
         day_df["pred"] = pred
-
+        
+        # обновление истории
         for row in day_df.itertuples():
             key = (row.store_nbr, row.item_nbr)
 
