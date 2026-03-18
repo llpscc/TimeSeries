2a9a764744d46750d7addfd41ecf8486990d19ca
llpscc <l.pascenco@gmail.com>
2026-03-18 19:58:23 +0200

    Delete src directory

--- src/metrics.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-def nwrmsle(y_true, y_pred, weights):
-    return np.sqrt(
-        np.sum(weights * (np.log1p(y_pred) - np.log1p(y_true)) ** 2)
-        / np.sum(weights)
-    )
