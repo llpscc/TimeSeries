af79ec0585f88523812918e04d1ceb0f9ee05cd1
llpscc <l.pascenco@gmail.com>
2026-03-18 14:34:09 +0000

    Добавлены новые функции

---
+++ pipelines/train.py
@@ -0,0 +1,20 @@
+from src.models import train_lgbm
+from src.inference import build_history, recursive_pred
+from src.metrics import nwrmsle
+
+
+def run_train(train_df, val_df, X_train, y_val, cat_cols):
+
+    model = train_lgbm(X_train, train_df["unit_sales"])
+
+    history = build_history(train_df)
+
+    pred_val = recursive_pred(val_df, X_train, model, history, cat_cols)
+
+    score = nwrmsle(
+        y_val.values,
+        pred_val["pred"].values,
+        pred_val["perishable"].values
+    )
+
+    return score
